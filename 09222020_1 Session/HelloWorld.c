ac486caf089b0f95d7cdb55ffdf3b47d68b9d9fe
mafaruk <mafaruk@users.noreply.github.com>
2020-11-26 02:40:50 +0530

    Create HelloWorld.c

---
+++ 09222020_1 Session/HelloWorld.c
@@ -0,0 +1,10 @@
+#include<stdio.h>// standard library contains method like puts(), printf(); 
+/* 
+Header section contain macros, 
+function declaraiton, data types.
+*/
+int main(){// main method, program execution start in main mehtod 
+	
+	printf("Hello world");// ptints text on console 
+	return 0; // function needs to return integer  value as we defined return type as int
+}//end of the funtion 
